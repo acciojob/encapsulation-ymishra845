e325a26847297bae045e61efcc5f6fd75663c7c5
Yogesh mishra <139787838+ymishra845@users.noreply.github.com>
2024-01-14 18:58:43 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+	
+	 public static void main(String[] args) {
+		 RWOnly objr= new RWOnly();
+		 
+		 objr.setName("Arjun");
+		 System.out.println(objr.getName());
+	 }
+
 }

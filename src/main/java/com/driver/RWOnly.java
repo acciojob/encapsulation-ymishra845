d3c53fb2175daa50d5a2f1bf70df79601c1f70ec
Yogesh mishra <139787838+ymishra845@users.noreply.github.com>
2024-01-14 18:59:49 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "RWOnly [name=" + name + "]";
+	}
+	
 }
